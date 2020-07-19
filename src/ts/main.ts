866a9991608e163ebb9b434202d3da51823d8c42
Alexandr Ilyushechkin <progruzovik@gmail.com>
2020-07-19 12:47:17 +0300

    add fetch

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,73 +1,77 @@
 import * as toWav from "audiobuffer-to-wav";
 
-let log = console.log.bind(console);
 const id = val => document.getElementById(val);
 const ul = id('ul');
 const gUMbtn = id('gUMbtn');
-const start = id('start') as HTMLButtonElement;
-let stopp = id('stop') as HTMLButtonElement;
-let stream;
+const btnStart = id('start') as HTMLButtonElement;
+let btnStop = id('stop') as HTMLButtonElement;
 let recorder;
 let counter = 1;
 let chunks;
 
-gUMbtn.onclick = e => {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(_stream => {
-        stream = _stream;
-        id('gUMArea').style.display = 'none';
-        id('btns').style.display = 'inherit';
-        start.removeAttribute('disabled');
-        recorder = new MediaRecorder(stream);
-        recorder.ondataavailable = e => {
-            chunks.push(e.data);
-            if (recorder.state === 'inactive') makeLink();
-        };
-        log('got media successfully');
-    }).catch(log);
-}
+gUMbtn.onclick = () => {
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(s => {
+            id('gUMArea').style.display = 'none';
+            id('btns').style.display = 'inherit';
+            btnStart.removeAttribute('disabled');
+            recorder = new MediaRecorder(s);
+            recorder.ondataavailable = e => {
+                chunks.push(e.data);
+                if (recorder.state === 'inactive') {
+                    makeLink();
+                }
+            };
+        });
+};
 
-start.onclick = e => {
-    start.disabled = true;
-    stopp.removeAttribute('disabled');
+btnStart.onclick = () => {
+    btnStart.disabled = true;
+    btnStop.removeAttribute('disabled');
     chunks = [];
     recorder.start();
-}
+};
 
-stopp.onclick = e => {
-    stopp.disabled = true;
+btnStop.onclick = () => {
+    btnStop.disabled = true;
     recorder.stop();
-    start.removeAttribute('disabled');
-}
+    btnStart.removeAttribute('disabled');
+};
 
-function makeLink(){
-    let blob = new Blob(chunks, { type: 'audio/ogg' });
+function makeLink() {
+    const blob = new Blob(chunks, { type: 'audio/ogg' });
 
     const context = new AudioContext({ sampleRate: 16000 });
     const reader = new FileReader();
 
     reader.onloadend = () => {
+        const myArrayBuffer = reader.result as ArrayBuffer;
 
-        let myArrayBuffer = reader.result as ArrayBuffer;
+        context.decodeAudioData(myArrayBuffer)
+            .then(ab => {
+                const wav = toWav(ab) as ArrayBuffer;
+                const wavBlob = new Blob([wav], { type: 'audio/wav' });
 
-        context.decodeAudioData(myArrayBuffer, (audioBuffer) => {
-            const wav = toWav(audioBuffer) as ArrayBuffer;
-            const wavBlob = new Blob([wav], { type: 'audio/wav' })
+                const url = URL.createObjectURL(wavBlob);
+                const li = document.createElement('li');
+                const mt = document.createElement('audio');
+                const hf = document.createElement('a');
 
-            let url = URL.createObjectURL(wavBlob), li = document.createElement('li'),
-                mt = document.createElement('audio'),
-                hf = document.createElement('a');
+                mt.controls = true;
+                mt.src = url;
+                hf.href = url;
+                hf.download = `${counter++}.wav`;
+                hf.innerHTML = `download ${hf.download}`;
+                li.appendChild(mt);
+                li.appendChild(hf);
+                ul.appendChild(li);
 
-            mt.controls = true;
-            mt.src = url;
-            hf.href = url;
-            hf.download = `${counter++}.wav`;
-            hf.innerHTML = `download ${hf.download}`;
-            li.appendChild(mt);
-            li.appendChild(hf);
-            ul.appendChild(li);
-        });
+                fetch("http://localhost:8080/speech", { method: "POST", body: wavBlob })
+                    .then(r => r.json())
+                    .then(d => console.log(d));
+            });
     };
 
-    //Load blob
     reader.readAsArrayBuffer(blob);
 }
